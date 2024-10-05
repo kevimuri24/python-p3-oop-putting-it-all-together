6e0cc079394dc9a30811d50fc18a5acedd5142f8
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-10-05 00:07:17 -0400

    Finished Lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__(self, title, page_count):
+        self.title = title  # This will call the setter for title
+        self.page_count = page_count  # This will call the setter for page_count
         
+        
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title="And Then There Were None"):
+        if isinstance(title, str):
+            self._title = title
+        else:
+            raise ValueError ("Title must be a string")
+        
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_count='not an integer'):
+        if isinstance(page_count, int) and page_count > 0:  # Ensure it's a positive integer
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+        
+book = Book("Factflullness", 600)
+print(book.title)  
+print(book.page_count)
+print(book.turn_page())
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,44 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand  # This will call the setter for brand
+        self.size = size    # This will call the setter for size
+        self._condition = "Used"  # Initial condition set to "Used"
+        
+    @property
+    def brand(self):
+        return self._brand
+    
+    @brand.setter
+    def brand(self, brand):
+        if isinstance(brand, str) and 1 <= len(brand) <= 50:  # Validate brand length
+            self._brand = brand
+        else:
+            print("brand must be a string")
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    @property
+    def condition(self):
+        return self._condition
+    
+    def cobble(self):  # Changed this to a method instead of a setter
+        self._condition = "New"  # Update condition to "New"
+        print("Your shoe is as good as new!")  # Print message for cobbling
+
+# Example usage
+shoes = Shoe("Puma", 10)
+print(shoes.brand)      # Outputs: Puma
+print(shoes.size)       # Outputs: 10
+shoes.cobble()          # Outputs: Your shoe is as good as new!
+print(shoes.condition)   # Outputs: New
